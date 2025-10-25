c193771cdde56f671a01fa828d5144e7722180b9
LucasMTC <lucas.callisperis@gmail.com>
2025-10-25 01:23:01 -0600

    Added env api key and .gitignore

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+
+
+app = FastAPI()
+
+@app.get("/")
 def main():
-    print("Hello from hackmty2025!")
+    return {"Hello": "hackmty2025!"}
 
 
 if __name__ == "__main__":
+    print(api_key)
     main()
