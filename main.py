955f856575479d65eb1e01797b7ebd51870bd2fe
LucasMTC <lucas.callisperis@gmail.com>
2025-10-25 05:37:50 -0600

    Added communication between Nessie and Supabase

--- main.py
+++ main.py
@@ -1,19 +1,83 @@
-from fastapi import FastAPI
+import requests
+import json
+import random
+from fastapi import FastAPI, HTTPException
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
+API_KEY = os.getenv("API_KEY")
+BASE_URL = "http://api.nessieisreal.com"
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SERVICE_ROLE = os.getenv("SUPABASE_SERVICE_ROLE")
 
-api_key = os.getenv("API_KEY")
+headers = {
+    "Content-Type": "application/json",
+    "apikey": SERVICE_ROLE,
+    "Authorization": f"Bearer {SERVICE_ROLE}",
+    "Prefer": "return=representation"
+}
 
+def create_customer():
+    params = {
+        "first_name": "John",
+        "last_name": "Doe",
+        "address": {
+            "street_number": "101",
+            "street_name": "Wall Street",
+            "city": "New York",
+            "state": "NY",
+            "zip": "10010"
+        }
+    }
+    response = requests.post(f"{BASE_URL}/customers?key={API_KEY}", json=params)
+    if response.status_code != 201:
+        print(f"Error: {response.status_code}\n {response.json()}")
+        return
+    
+    endpoint = f"{SUPABASE_URL}/rest/v1/customer"
+    payload = response.json()["objectCreated"]
+    payload["customer_id"] = payload["_id"]
+    payload["city"] = payload["address"]["city"]
+    payload.pop("_id")
+    payload.pop("address")
+    
+
+    resp = requests.post(endpoint, headers=headers, json=payload)
+    if resp.status_code != 201:
+        print("[customer] status:", resp.status_code)
+    
+
+def create_account(customer_id):
+    params = {
+        "type": "Credit Card",
+        "nickname": "Personal Credit Card",
+        "rewards": 0,
+        "balance": 10000
+    }
+    response = requests.post(f"{BASE_URL}/customers?key={API_KEY}", json=params)
+    if response.status_code == 201:
+        print(response.json())
+    else:
+        print(f"Error: {response.status_code}\n {response.json()}")
 
 app = FastAPI()
 
-@app.get("/")
-def main():
-    return {"Hello": "hackmty2025!"}
+@app.get("/user/{user_id}")
+def get_user_accounts(user_id: str):
+    accounts = {user_id:[]}
+    response = requests.get(f"{BASE_URL}/accounts?key={API_KEY}")
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail=response.text)
+    for acc in response.json():
+        if acc["customer_id"] == user_id:
+            accounts[user_id].append(acc)
+    return accounts
+
+
 
 
 if __name__ == "__main__":
-    print(api_key)
-    main()
+    print("Application Started")
+    create_customer()
+
